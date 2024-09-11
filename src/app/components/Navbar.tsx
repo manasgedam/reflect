b6cb4313899f82295ee5f9da04ea01c263575c4e
Manas <manasgedam26@gmail.com>
2024-09-11 16:24:37 +0530

    Added: Navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+import Image from "next/image"
+import logo from "@/app/assets/images/logo.png"
+import {Button} from "@/components/ui/button"
+
+export default function Navbar() {
+  return (
+    <nav className="border-b flex justify-center">
+      <div className="container flex items-center justify-between py-1">
+        <Link href="/">
+          <Image
+            src={logo}
+            width={70}
+            height={70}
+            alt="logo"
+          />
+        </Link>
+        <div className="hidden space-x-6 md:flex">
+          <Link href="/about" className="text-md font-medium transition-colors hover:text-primary">
+            About
+          </Link>
+          <Link href="/features" className="text-md font-medium transition-colors hover:text-primary">
+            Features
+          </Link>
+          <Link href="/contact" className="text-md font-medium transition-colors hover:text-primary">
+            Contact
+          </Link>
+        </div>
+
+        <div className=" space-x-2 md:flex">
+          <Button variant="outline">Sign In</Button>
+          <Button>Sign Up</Button>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Figtree } from "next/font/google"
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const figtree = Figtree({
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
@@ -26,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${figtree.className} antialiased`}
       >
         {children}
       </body>
