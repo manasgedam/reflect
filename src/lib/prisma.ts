43facd813b23ccb8a370f0cdaa17742eeec626b6
Manas <manasgedam26@gmail.com>
2024-09-15 16:18:23 +0530

    Deploy: Attempt#2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from "@neondatabase/serverless"
+import { PrismaNeon } from "@prisma/adapter-neon"
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL })
+  const adapter = new PrismaNeon(neon)
+  return new PrismaClient({ adapter })
 }
 
 declare const globalThis: {
