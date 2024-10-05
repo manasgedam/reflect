c1ae103447ca63743ed41bb40c3a12610361749b
Manas <manasgedam26@gmail.com>
2024-10-05 23:20:46 +0530

    Authentication: Completed & Deployment Attempt #7

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
 export { auth as middleware } from "@/auth";
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|.*\\.mp4$).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|.*\\.mp4|png$).*)"],
 };
